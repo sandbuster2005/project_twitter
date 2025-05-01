8d89a1dded071f25c6695b48752669f68463db42
rickarockFR <florianquiedeville@pop-os.home>
2025-05-01 18:32:04 +0200

    changé système sauts + dézoomé cam

--- Scripts/Characters/player.gd
+++ Scripts/Characters/player.gd
@@ -7,10 +7,12 @@ class_name Player extends CharacterBody2D
 @export_category("Jump")
 @export var Jump_strength = 1500
 @export var Dash_strength = 1205
+@export var jumpMax = 2
 @onready var Dash_cooldown : float = 0
 
 var Move = true 
-var dash = 0
+var jumpNum = jumpMax
+
 @onready var gravity = VariableManager.gravity
 
 func _ready() -> void:
@@ -18,11 +20,21 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Move:
-		if not is_on_floor() and Dash_cooldown<20 and velocity.y <= 3000:
+		if is_on_floor():
+			if jumpNum == -1 and Input.is_action_pressed("Up"):
+				jumpNum = jumpMax - 1
+				velocity.y = -Jump_strength
+			else:
+				jumpNum = jumpMax
+		elif Dash_cooldown<20 and velocity.y <= 3000:
 			velocity.y += gravity
 		
-		if Input.is_action_pressed("Up") and is_on_floor():
-			velocity.y = -Jump_strength
+		if Input.is_action_just_pressed("Up"):
+			if jumpNum > 0:
+				jumpNum -= 1
+				velocity.y = -Jump_strength
+			else:
+				jumpNum = -1
 		
 		if Dash_cooldown !=0 :
 			Dash_cooldown -= 1
