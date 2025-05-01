acc1164345a22c26fdb05cfd2937864628b0bd14
rickarockFR <florianquiedeville@pop-os.home>
2025-05-01 17:20:44 +0200

    Changement des physiques du joueur

--- Scripts/Characters/player.gd
+++ Scripts/Characters/player.gd
@@ -1,13 +1,12 @@
 class_name Player extends CharacterBody2D
 
 @export_category("Walk/Run")
-@export var Speed : int = 505
-@export var Acceleration : int = 3000
+@export var Speed : int = 700
+@export var Acceleration : int = 5600
 
 @export_category("Jump")
-@export var Jump_strength = 1205
+@export var Jump_strength = 1500
 @export var Dash_strength = 1205
-@export var Jump_sound : AudioStreamPlayer2D
 @onready var Dash_cooldown : float = 0
 
 var Move = true 
@@ -18,7 +17,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Move:
-		if not is_on_floor():
+		if not is_on_floor() and Dash_cooldown<20 and velocity.y <= 3000:
 			velocity.y += gravity
 		
 		if Input.is_action_pressed("Up") and is_on_floor():
@@ -31,11 +30,12 @@ func _process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, direction, Acceleration * delta)
 		
 		if Input.is_action_pressed("dash") and not Dash_cooldown:
-			if velocity.x > 0:
+			if Input.get_axis("Left", "Right") > 0:
 				velocity.x += Dash_strength 
 			else :
 				velocity.x -= Dash_strength
 			Dash_cooldown = 30
+			velocity.y = 0
 		
 		
 		move_and_slide()
