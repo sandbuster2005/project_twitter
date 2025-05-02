290af2442c9a7f151378d4acdbad524ede265d55
ToT M <robloxit14@gmail.com>
2025-05-02 19:59:25 +0200

    De la merde

--- Scripts/Characters/Ennemies/ennemy.gd
+++ Scripts/Characters/Ennemies/ennemy.gd
@@ -4,11 +4,13 @@ extends CharacterBody2D
 var gravity = VariableManager.gravity
 var direction = 1
 func _process(delta: float) -> void:
-	
+	if randi() % 100 > 95:
+		direction *= -1
 	if not is_on_floor():
 		velocity.y += gravity
-		direction *= -1
+		
 	if is_on_wall():
 		direction *= -1
+		
 	velocity.x = move_toward(velocity.x, direction * Speed, Acceleration * delta)
 	move_and_slide()
