f9090300d72229dd5d5981c776ed6867b7633b81
sandbuster2005 <dorengebenjamin@gmail.com>
2025-05-02 19:05:51 +0200

    upadted enemy movement

--- Scripts/Characters/Ennemies/ennemy.gd
+++ Scripts/Characters/Ennemies/ennemy.gd
@@ -8,8 +8,6 @@ func _process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity
 		direction *= -1
-	if not RayCast2D.is_colliding:
-		direction *= -1
 	if is_on_wall():
 		direction *= -1
 	velocity.x = move_toward(velocity.x, direction * Speed, Acceleration * delta)
